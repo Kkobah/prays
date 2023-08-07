e81d81ab74ed04416a9855d2a3e706c1b70072a6
Kkobah <137084218+Kkobah@users.noreply.github.com>
2023-08-08 00:55:32 +0300

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -1,36 +1,26 @@
 import random
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+import telebot
 
 # Replace 'YOUR_TOKEN' with your actual Telegram Bot API token
 TOKEN = '6357249652:AAGrVUHOKYWcLrBJr8KsZ4-cbX4BwKQYmmc'
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('مرحبًا! أنا روبوت أذكار وأوقات الصلاة. ارسل /praytimes للحصول على أوقات الصلاة.')
+bot = telebot.TeleBot(TOKEN)
 
-def pray_times(update: Update, context: CallbackContext) -> None:
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'مرحبًا! أنا روبوت أذكار وأوقات الصلاة. ارسل /praytimes للحصول على أوقات الصلاة.')
+
+@bot.message_handler(commands=['praytimes'])
+def pray_times(message):
     # Replace these prayer times with actual prayer times
     prayer_times = "أوقات الصلاة:\nفجر: 04:30 AM\nشروق الشمس: 05:45 AM\nظهر: 12:15 PM\nعصر: 04:00 PM\nمغرب: 07:30 PM\nعشاء: 09:00 PM"
-    update.message.reply_text(prayer_times)
+    bot.reply_to(message, prayer_times)
 
-def send_dua(update: Update, context: CallbackContext) -> None:
+@bot.message_handler(func=lambda message: True)
+def send_dua(message):
     # Replace these with actual duas
     duas = ["اللهم اغفر لي وللمؤمنين والمؤمنات", "اللهم إني أسألك علمًا نافعًا ورزقًا طيبًا وعملاً متقبلاً"]
     selected_dua = random.choice(duas)
-    update.message.reply_text(selected_dua)
+    bot.reply_to(message, selected_dua)
 
-def main() -> None:
-    updater = Updater(TOKEN)
-
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("praytimes", pray_times))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, send_dua))
-
-    updater.start_polling()
-
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+bot.polling()
