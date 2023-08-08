4e509d413a412fb90fc86fc023df79adaf84d6e3
Kkobah <137084218+Kkobah@users.noreply.github.com>
2023-08-08 16:23:00 +0300

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -1,30 +1,74 @@
 import random
 import telebot
+import sqlite3
+from telebot import types
+
 # Replace 'YOUR_TOKEN' with your actual Telegram Bot API token
 TOKEN = '6357249652:AAGrVUHOKYWcLrBJr8KsZ4-cbX4BwKQYmmc'
+
 bot = telebot.TeleBot(TOKEN)
+
+conn = sqlite3.connect('data.db')
+cursor = conn.cursor()
+
+# Create a table for azkar if it doesn't exist
+cursor.execute('''CREATE TABLE IF NOT EXISTS azkar (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT)''')
+
+# Create a table for duas if it doesn't exist
+cursor.execute('''CREATE TABLE IF NOT EXISTS duas (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT)''')
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'مرحبًا! أنا روبوت أذكار وأوقات الصلاة وأدعية. ارسل /azkar للحصول على أذكار، /dua للحصول على أدعية، و /praytimes للحصول على أوقات الصلاة.')
-@bot.message_handler(commands=['pr'])
+    bot.reply_to(message, 'مرحبًا! أنا روبوت أذكار وأدعية. ارسل /azkar للحصول على أذكار، /dua للحصول على أدعية، و /praytimes للحصول على أوقات الصلاة.')
+
+@bot.message_handler(commands=['praytimes'])
 def pray_times(message):
     # Replace these prayer times with actual prayer times for Baghdad
-    prayer_times = "أوقات الصلاة في بغداد:\nفجر: 03:45 AM\nشروق الشمس: 05:18 AM\nظهر: 12:15 PM\nعصر: 03:57 PM\nمغرب: 07:00 PM\nعشاء: 08:28 PM"
+    prayer_times = "أوقات الصلاة في بغداد:\nفجر: 04:30 AM\nشروق الشمس: 05:45 AM\nظهر: 12:15 PM\nعصر: 04:00 PM\nمغرب: 07:30 PM\nعشاء: 09:00 PM"
     bot.reply_to(message, prayer_times)
 
 @bot.message_handler(commands=['azkar'])
-@bot.message_handler(commands=['a'])
 def send_azkar(message):
-    # Replace these with actual azkar
-    azkar = ["رَضيـتُ بِاللهِ رَبَّـاً وَبِالإسْلامِ ديـناً وَبِمُحَـمَّدٍ صلى الله عليه وسلم نَبِيّـاً" ,"سُبْحَانَ اللَّهِ وَبِحَمْدِهِ", "لا إِلَهَ إِلَّا اللهُ وَحْدَهُ لا شَرِيكَ لَهُ"]
-    selected_azkar = random.choice(azkar)
-    bot.reply_to(message, selected_azkar)
+    cursor.execute("SELECT text FROM azkar")
+    azkar_list = cursor.fetchall()
+    
+    if not azkar_list:
+        bot.reply_to(message, "لا يوجد أذكار مضافة حاليًا.")
+    else:
+        selected_azkar = random.choice(azkar_list)[0]
+        bot.reply_to(message, selected_azkar)
 
 @bot.message_handler(commands=['dua'])
-@bot.message_handler(commands=['d'])
 def send_dua(message):
-    # Replace these with actual duas
-    duas = ["رَبِّ اغْفِرْ وَارْحَمْ وَأَنتَ خَيْرُ الرَّاحِمِينَ", "اللَّهُمَّ إِنِّي أَعُوذُ بِكَ مِنْ زَوَالِ نِعْمَتِكَ وَتَحَوُّلِ عَافِيَتِكَ وَفُجَاءَةِ نِقْمَتِكَ وَجَمِيعِ سَخَطِكَ"]
-    selected_dua = random.choice(duas)
-    bot.reply_to(message, selected_dua)
+    cursor.execute("SELECT text FROM duas")
+    duas_list = cursor.fetchall()
+    
+    if not duas_list:
+        bot.reply_to(message, "لا يوجد أدعية مضافة حاليًا.")
+    else:
+        selected_dua = random.choice(duas_list)[0]
+        bot.reply_to(message, selected_dua)
+
+@bot.message_handler(commands=['addazkar'])
+def add_azkar(message):
+    bot.reply_to(message, "أرسل الذكر الجديد:")
+    bot.register_next_step_handler(message, save_new_azkar)
+
+def save_new_azkar(message):
+    new_azkar = message.text
+    cursor.execute("INSERT INTO azkar (text) VALUES (?)", (new_azkar,))
+    conn.commit()
+    bot.reply_to(message, "تم إضافة الذكر بنجاح.")
+
+@bot.message_handler(commands=['adddua'])
+def add_dua(message):
+    bot.reply_to(message, "أرسل الدعاء الجديد:")
+    bot.register_next_step_handler(message, save_new_dua)
+
+def save_new_dua(message):
+    new_dua = message.text
+    cursor.execute("INSERT INTO duas (text) VALUES (?)", (new_dua,))
+    conn.commit()
+    bot.reply_to(message, "تم إضافة الدعاء بنجاح.")
+
 bot.polling()
